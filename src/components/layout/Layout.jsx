13b740e727ed6d02614279aa0370acbe3480a7a2
zee <mohammadzidane058@gmail.com>
2026-02-11 10:02:09 +0700

    fix: grid

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 export default function Layout({ childern }) {
     return (
         <>
-            <div className="w-full min-h-max bg-gray-100 flex font-sans">
+            <div className="w-full min-h-screen bg-gray-100 flex font-sans">
                 {childern}
             </div>
         </>
